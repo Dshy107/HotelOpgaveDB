7d85f1997122eceb31e594adb9ec976285209fd8
Dshy <KortSkg@KORTSKG-PC>
2017-03-14 09:20:11 +0100

    Har forbundet til databasen og kaldt alt info ud om hotel og gæster

--- HotelOpgaveDB/Program.cs
+++ HotelOpgaveDB/Program.cs
@@ -10,6 +10,29 @@ namespace HotelOpgaveDB
     {
         static void Main(string[] args)
         {
+            
+
+            using (var db = new HotelContext())
+            {
+                
+                var Hotellist = from h in db.Hotel
+                               
+                                select h;
+                Console.WriteLine("All hotel info: ");
+                foreach (var h in Hotellist)
+                {
+                    Console.WriteLine(h.ToString());
+                }
+                var Guestlist = from g in db.Guest
+
+                                select g;
+                Console.WriteLine("All guest info: ");
+                foreach (var g in Guestlist)
+                {
+                    Console.WriteLine(g.ToString());
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
